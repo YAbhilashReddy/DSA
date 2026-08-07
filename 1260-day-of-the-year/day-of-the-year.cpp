8976ffebc58bd179c61b52e270a58eb4e6fa401e
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-08-07 19:05:39 +0530

    Time: 52 ms (5.01%) | Memory: 12.5 MB (12.73%) - LeetSync

--- 1260-day-of-the-year/day-of-the-year.cpp
+++ 1260-day-of-the-year/day-of-the-year.cpp
@@ -7,7 +7,7 @@ public:
         if((year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) && months > 2) total++;
         cout << total << endl;
         for(int i=1 ; i<months ; i++){
-            if(i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 31) total += 31;
+            if(i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 ) total += 31;
             else if(i == 4 || i == 6 || i == 9 || i == 11) total += 30;
             else if(i == 2) total += 28;
         }
