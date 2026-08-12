9ea7153d7111a31009cda9e062b2feb7bc248a72
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-08-12 15:28:39 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (64.17%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int val = 0 , n = *max_element(citations.begin() , citations.end());
-        for(int i=0 ; i<=n ; i++){
-            int c = 0;
-            for(int j:citations) c += (j >= i);
-            val = (c >= i && c > val ? i : val);
+        sort(citations.begin() , citations.end());
+        int n = citations.size();
+        for(int i=0 ; i<n ; i++){
+            if(citations[i] >= n-i) return n-i;
         }
-        return val;
+        return 0;
+
+        //int val = 0 , n = *max_element(citations.begin() , citations.end());
+        //for(int i=0 ; i<=n ; i++){
+        //    int c = 0;
+        //    for(int j:citations) c += (j >= i);
+        //    val = (c >= i && c > val ? i : val);
+        //}
+        //return val;
     }
 };
