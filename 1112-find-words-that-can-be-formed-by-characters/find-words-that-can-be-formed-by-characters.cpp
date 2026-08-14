ddee5fdc90551b049ed95ea12558b318fef54806
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-08-14 12:59:36 +0530

    Time: 3 ms (90.90%) | Memory: 19 MB (96.03%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int countCharacters(vector<string>& words, string chars) {
-        vector<int> alpha(26,0);
-        for(char C:chars) alpha[C - 'a']++;
-        int count = 0;
-        for(auto word:words){
-            vector<int> ALPHA = alpha;
-            int cou = 0;
-            for(char c:word){
+        array<int, 26> available{};
+        for (char c : chars) available[c - 'a']++;
+        int ans = 0;
+        for (const string& word : words) {
+            array<int, 26> used{};
+            bool possible = true;
+            for (char c : word) {
                 int i = c - 'a';
-                if(ALPHA[i] == 1) cou++ , ALPHA[i] = 0;
-                else if(ALPHA[i] > 1) cou++ , ALPHA[i]--;
+                if (++used[i] > available[i]) {
+                    possible = false;
+                    break;
+                }
             }
-            if(cou == word.length()) count += cou;
+            if (possible)
+                ans += word.size();
         }
-        return count;
+        return ans;
     }
 };
