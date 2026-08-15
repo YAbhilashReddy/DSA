468438843590a8726826abed94934fdcc79e2da6
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-08-15 13:07:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (81.21%) - LeetSync

--- 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.cpp
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     string replaceDigits(string s) {
-        string ans = "";
-        for(char c:s){
-            if(c >= 'a' && c <= 'z') ans.push_back(c);
-            else {
-                char C = ans.back() + (c - '0');
-                ans.push_back(C);
-            }
-        }
-        return ans;
+        for(int i=1 ; i<s.length() ; i+=2) s[i] += s[i - 1] - '0';
+        return s;
+
+
+
+        //string ans = "";
+        //for(char c:s){
+        //    if(c >= 'a' && c <= 'z') ans.push_back(c);
+        //    else {
+        //        char C = ans.back() + (c - '0');
+        //        ans.push_back(C);
+        //    }
+        //}
+        //return ans;
     }
 };
