938704c226a57309be25cd31cedd7fce66e42da2
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-08-22 23:33:33 +0530

    Time: 14 ms (74.12%) | Memory: 25 MB (71.07%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<vector<int> , vector<string>> mp;
-        for(string str:strs){
-            vector<int> arr(26 , 0);
-            for(char c:str) arr[c - 'a']++;
-            mp[arr].push_back(str);
-        }
-        vector<vector<string>> ans;
-        for(auto it:mp) ans.push_back(it.second);
-        return ans;
-
-        //unordered_map<string , vector<string>> ans;
-        //for(string &str:strs){
-        //    string s = str;
-        //    sort(s.begin() , s.end());
-        //    ans[s].push_back(str);
+        //map<vector<int> , vector<string>> mp;
+        //for(string str:strs){
+        //    vector<int> arr(26 , 0);
+        //    for(char c:str) arr[c - 'a']++;
+        //    mp[arr].push_back(str);
         //}
-        //vector<vector<string>> res;
-        //for(auto &i:ans) res.push_back(i.second);
-        //return res;
+        //vector<vector<string>> ans;
+        //for(auto it:mp) ans.push_back(it.second);
+        //return ans;
+
+        unordered_map<string , vector<string>> ans;
+        for(string &str:strs){
+            string s = str;
+            sort(s.begin() , s.end());
+            ans[s].push_back(str);
+        }
+        vector<vector<string>> res;
+        for(auto &i:ans) res.push_back(i.second);
+        return res;
     }
 };
