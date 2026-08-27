62fe490ba3e6dab3670bbff3ca162d7b02ff78d2
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-08-27 10:30:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (91.45%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
-        stringstream S1(s1) , S2(s2);
-        string word;
         unordered_map<string,int> freq;
-        while(S1 >> word) freq[word]++;
-        while(S2 >> word) freq[word]++;
+        string s = "";
+        for(char c:s1){
+            if(c == ' ') freq[s]++ , s = "";
+            else s.push_back(c);
+        }
+        freq[s]++ , s = "";
+        for(char c:s2){
+            if(c == ' ') freq[s]++ , s = "";
+            else s.push_back(c); 
+        }
+        freq[s]++;
         vector<string> ans;
-        for(auto[k,v]:freq){
+        for(auto [k,v]:freq){
             if(v == 1) ans.push_back(k);
         }
         return ans;
+
+
+
+        //stringstream S1(s1) , S2(s2);
+        //string word;
+        //unordered_map<string,int> freq;
+        //while(S1 >> word) freq[word]++;
+        //while(S2 >> word) freq[word]++;
+        //vector<string> ans;
+        //for(auto[k,v]:freq){
+        //    if(v == 1) ans.push_back(k);
+        //}
+        //return ans;
     }
 };
