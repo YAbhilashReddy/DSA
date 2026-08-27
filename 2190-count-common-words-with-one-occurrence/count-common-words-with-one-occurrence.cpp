59ebc9a50850ab3cc63eb3aea9d8b4328f5fd782
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-08-27 10:41:25 +0530

    Time: 16 ms (36.79%) | Memory: 22.7 MB (58.68%) - LeetSync

--- 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.cpp
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int countWords(vector<string>& words1, vector<string>& words2) {
         unordered_map<string,int> freq1 , freq2;
-        for(string s:words1) freq1[s]++;
-        for(string s:words2) freq2[s]++;
+        for(string &s:words1) freq1[s]++;
+        for(string &s:words2) freq2[s]++;
         int c = 0;
-        for(auto [k,v]:freq1) c += (v == 1 && freq2[k] == 1);
+        for(auto &[k,v]:freq1) c += (v == 1 && freq2[k] == 1);
         return c;
     }
 };
