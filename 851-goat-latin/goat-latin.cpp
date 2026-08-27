a89e89f1da19f16d2880afcb29b028f8e4991d6d
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-08-27 10:16:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (69.79%) - LeetSync

--- 851-goat-latin/goat-latin.cpp
+++ 851-goat-latin/goat-latin.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     string toGoatLatin(string sentence) {
-        stringstream ss(sentence);
-        vector<string> words;
-        string word;
-        while(ss >> word) words.push_back(word);
-        string A = "" , vowels = "aeiouAEIOU" , ans = "";
-        for(string wor:words){
+        stringstream ss (sentence);
+        string word , ans , A = "" , vowels = "aeiouAEIOU";
+        while(ss >> word){
             A.push_back('a');
-            char c = wor[0];
-            if(vowels.find(c) == string::npos) {
-                ans += wor.substr(1,wor.length()-1);
-                ans.push_back(c) , ans += ("ma" + A + " ");
-            } else ans += wor ,  ans += ("ma" + A + " ");
+            char c = word[0];
+            if(vowels.find(c) == string::npos){
+                ans += word.substr(1,word.length()-1);
+                ans.push_back(c) , ans += ("ma" + A + ' ');
+            } else ans += word , ans += ("ma" + A + ' ');
         }
         ans.pop_back();
         return ans;
+
+
+
+        //stringstream ss(sentence);
+        //vector<string> words;
+        //string word;
+        //while(ss >> word) words.push_back(word);
+        //string A = "" , vowels = "aeiouAEIOU" , ans = "";
+        //for(string wor:words){
+        //    A.push_back('a');
+        //    char c = wor[0];
+        //    if(vowels.find(c) == string::npos) {
+        //        ans += wor.substr(1,wor.length()-1);
+        //        ans.push_back(c) , ans += ("ma" + A + " ");
+        //    } else ans += wor ,  ans += ("ma" + A + " ");
+        //}
+        //ans.pop_back();
+        //return ans;
     }
 };
