4966d463bcc4d792b6068a8d964f26c16c96e78e
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-01 07:32:01 +0530

    Time: 11 ms (91.76%) | Memory: 41.4 MB (29.74%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
@@ -2,16 +2,35 @@ class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
         unordered_map<string,int> freq;
+        for(int i = 0 ; i < list1.size() ; i++) freq[list1[i]] = i;
+        vector<string> ans;
         int mini = INT_MAX;
-        for(int i=0 ; i<list1.size() ; i++){
-            for(int j=0 ; j<list2.size() ; j++){
-                if(list1[i] == list2[j]) mini = min(mini , i+j) , freq[list1[i]] = (i+j);
+        for(int i = 0 ; i < list2.size() ; i++){
+            if(freq.count(list2[i])){
+                int sum = freq[list2[i]] + i;
+                if(sum < mini) mini = sum , ans.clear() , ans.push_back(list2[i]);
+                else if(sum == mini) ans.push_back(list2[i]);
             }
         }
-        vector<string> ans;
-        for(auto [k,v]:freq){
-            if(v == mini) ans.push_back(k);
-        }
         return ans;
+
+
+
+
+
+
+
+        //unordered_map<string,int> freq;
+        //int mini = INT_MAX;
+        //for(int i=0 ; i<list1.size() ; i++){
+        //    for(int j=0 ; j<list2.size() ; j++){
+        //        if(list1[i] == list2[j]) mini = min(mini , i+j) , freq[list1[i]] = (i+j);
+        //    }
+        //}
+        //vector<string> ans;
+        //for(auto [k,v]:freq){
+        //    if(v == mini) ans.push_back(k);
+        //}
+        //return ans;
     }
 };
