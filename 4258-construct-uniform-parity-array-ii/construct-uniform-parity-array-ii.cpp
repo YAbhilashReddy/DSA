4d95fb7f33fcf26cf41016435ef0e5ce70048755
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-03 06:58:19 +0530

    Time: 0 ms (100.00%) | Memory: 165.9 MB (31.94%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int n = nums1.size() , even = 0 , odd = 0 , mini = INT_MAX;
-        for(int i:nums1){
-            even += (i % 2 == 0) , odd += (i % 2) , mini = min(mini , i);
+        int minOdd = INT_MAX;
+        for(int i:nums1) {
+            if(i % 2 == 1) minOdd = min(minOdd , i);
         }
-        if(even != n && odd != n) {
-            for(int i=0 ; i<n ; i++){
-                if(nums1[i] % 2 == 0 && (nums1[i] - mini) % 2 == 0) return false;
-            }
+        for(int i:nums1) {
+            if(i % 2 == 0 && minOdd != INT_MAX && i < minOdd) return false;
         }
         return true;
+
+
+
+
+        // int n = nums1.size() , even = 0 , odd = 0 , mini = INT_MAX;
+        // for(int i:nums1){
+        //     even += (i % 2 == 0) , odd += (i % 2) , mini = min(mini , i);
+        // }
+        // if(even != n && odd != n) {
+        //     for(int i=0 ; i<n ; i++){
+        //         if(nums1[i] % 2 == 0 && (nums1[i] - mini) % 2 == 0) return false;
+        //     }
+        // }
+        // return true;
     }
 };
