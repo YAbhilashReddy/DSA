48666e99f366406631d2c7a8c3c772dde2ab659a
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-04 06:51:13 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (21.22%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
-        int total = 0;
-        for(int i=0 ; i<nums.size() ; i+= 2) total += (min(nums[i] , nums[i+1]));
-        return total;
+        vector<int> arr(20001 , 0);
+        for(int i:nums) arr[i + 10000]++;
+        int sum = 0 , pick = 1;
+        for(int i=0 ; i<20001 ; i++){
+            while(arr[i]){
+                if(pick) sum += i - 10000;
+                arr[i]--;
+                pick = !pick;
+            }
+        }
+        return sum;
+
+
+        // sort(nums.begin() , nums.end());
+        // int total = 0;
+        // for(int i=0 ; i<nums.size() ; i+= 2) total += nums[i];
+        // return total;
     }
 };
