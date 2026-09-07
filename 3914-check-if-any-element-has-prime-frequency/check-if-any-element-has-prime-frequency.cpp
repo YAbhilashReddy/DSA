0f02af07e5985391ff297cd86afb306ca85e9446
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-07 07:06:01 +0530

    Time: 0 ms (100.00%) | Memory: 24.3 MB (72.00%) - LeetSync

---
+++ 3914-check-if-any-element-has-prime-frequency/check-if-any-element-has-prime-frequency.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkPrime(int n){
+        if(n < 2) return false;
+        int c = 0;
+        for(int i=2 ; i*i<=n ; i++){
+            if(n % i == 0) return false;
+        }
+        return !c;
+    }
+
+    bool checkPrimeFrequency(vector<int>& nums) {
+        vector<int> arr(101);
+        for(int i:nums) arr[i]++;
+        for(int i:arr){
+            if(checkPrime(i)) return true;
+        }
+        return false;
+    }
+};
