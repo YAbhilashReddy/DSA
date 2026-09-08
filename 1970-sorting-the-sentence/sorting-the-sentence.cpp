11619df07a2432ce8822322b33be4e94fd42d457
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-08 07:33:12 +0530

    Time: 3 ms (41.57%) | Memory: 8.5 MB (42.43%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.cpp
+++ 1970-sorting-the-sentence/sorting-the-sentence.cpp
@@ -3,17 +3,31 @@ public:
     string sortSentence(string s) {
         stringstream ss(s);
         string word;
-        vector<string> arr(s.length() , "");
+        map<int,string> freq;
         while(ss >> word){
             int n = word.back() - '0';
             word.erase(word.length()-1 , 1);
-            arr[n-1] = word;
+            freq[n] = word;
         }
         string ans = "";
-        for(string x:arr) {
-            if(!x.empty()) ans += x + ' ';
-        }
+        for(auto[k,v]:freq) ans += v + ' ';
         ans.pop_back();
         return ans;
+
+
+        // stringstream ss(s);
+        // string word;
+        // vector<string> arr(s.length() , "");
+        // while(ss >> word){
+        //     int n = word.back() - '0';
+        //     word.erase(word.length()-1 , 1);
+        //     arr[n-1] = word;
+        // }
+        // string ans = "";
+        // for(string x:arr) {
+        //     if(!x.empty()) ans += x + ' ';
+        // }
+        // ans.pop_back();
+        // return ans;
     }
 };
