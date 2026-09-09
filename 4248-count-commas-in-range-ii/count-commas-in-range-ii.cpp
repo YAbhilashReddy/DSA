9e656ce85292203f7cda4e7fe98426f99b50f393
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-09 06:05:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (99.20%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0 , base = 1000;
+        while(base <= n){
+            ans += (n - base + 1);
+            base *= 1000;
+        }
+        return ans;
+    }
+};
