28ed47e07d02f63d6570e74d917c6314a66d0523
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-10 06:44:00 +0530

    Time: 2 ms (2.68%) | Memory: 8.1 MB (22.27%) - LeetSync

--- 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.cpp
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     int minimumSum(int num) {
-        string s = to_string(num);
-        sort(s.begin() , s.end());
-        int a = (s[0] - '0') * 10  + (s[2] - '0') , b = (s[1] - '0') * 10 + (s[3] - '0');
+        vector<int> arr(10,0);
+        while(num) arr[num % 10]++ , num /= 10;
+        vector<int> four(4,0);
+        int i=0;
+        for(int j=0 ; j<=9 ; j++){
+            while(arr[j]--) four[i++] = j;
+        }
+        int a = four[0] * 10 + four[2] , b = four[1] * 10 + four[3];
         return a + b;
+
+        // string s = to_string(num);
+        // sort(s.begin() , s.end());
+        // int a = (s[0] - '0') * 10  + (s[2] - '0') , b = (s[1] - '0') * 10 + (s[3] - '0');
+        // return a + b;
     }
 };
