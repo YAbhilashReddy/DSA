ab622c44b8faec132bb8d2b95a9dec9ddc2706f6
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-10 06:38:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (57.25%) - LeetSync

--- 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.cpp
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -3,11 +3,7 @@ public:
     int minimumSum(int num) {
         string s = to_string(num);
         sort(s.begin() , s.end());
-        string a = "" , b = "";
-        for(int i=0 ; i<s.length() ; i++){
-            if(i % 2) a.push_back(s[i]);
-            else b.push_back(s[i]);
-        }
-        return stoi(a) + stoi(b) ;
+        int a = (s[0] - '0') * 10  + (s[2] - '0') , b = (s[1] - '0') * 10 + (s[3] - '0');
+        return a + b;
     }
 };
