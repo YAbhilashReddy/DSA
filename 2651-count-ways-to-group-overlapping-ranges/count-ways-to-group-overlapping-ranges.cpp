ea1ff0e31fb37d4405bbcecdefdd926413ce2463
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-11 15:23:26 +0530

    Time: 27 ms (95.08%) | Memory: 76.9 MB (79.55%) - LeetSync

--- 2651-count-ways-to-group-overlapping-ranges/count-ways-to-group-overlapping-ranges.cpp
+++ 2651-count-ways-to-group-overlapping-ranges/count-ways-to-group-overlapping-ranges.cpp
@@ -2,13 +2,24 @@ class Solution {
 public:
     int countWays(vector<vector<int>>& ranges) {
         sort(ranges.begin() , ranges.end());
-        vector<vector<int>> ans;
+        int group = 0 , maxVal = -1;
         for(auto &range:ranges){
-            if(ans.empty() || ans.back()[1] < range[0]) ans.push_back(range);
-            else ans.back()[1] = max(ans.back()[1] , range[1]);
+            if(range[0] > maxVal) group++;
+            maxVal = max(maxVal , range[1]);
         }
         int res = 1 , mod = 1e9 + 7;
-        for(int i=0 ; i<ans.size() ; i++) res = (res * 2) % mod;
+        for(int i=0 ; i<group ; i++) res = (res * 2) % mod;
         return res;
+
+
+        //sort(ranges.begin() , ranges.end());
+        //vector<vector<int>> ans;
+        //for(auto &range:ranges){
+        //    if(ans.empty() || ans.back()[1] < range[0]) ans.push_back(range);
+        //    else ans.back()[1] = max(ans.back()[1] , range[1]);
+        //}
+        //int res = 1 , mod = 1e9 + 7;
+        //for(int i=0 ; i<ans.size() ; i++) res = (res * 2) % mod;
+        //return res;
     }
 };
