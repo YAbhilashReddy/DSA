5e010164cff533811822346a8e826e2b634005f7
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-11 14:39:03 +0530

    Time: 8 ms (24.66%) | Memory: 24.8 MB (11.54%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -3,12 +3,22 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin() , intervals.end());
         int i = 0;
-        while(i < intervals.size()-1){
-            if(intervals[i][1] >= intervals[i+1][0]){
-                intervals[i][1] = max(intervals[i][1] , intervals[i+1][1]);
-                intervals.erase(intervals.begin() + (i+1));
-            } else i++;
+        vector<vector<int>> ans;
+        for(auto interval:intervals){
+            if(ans.empty() || ans.back()[1] < interval[0]) ans.push_back(interval);
+            else ans.back()[1] = max(ans.back()[1] , interval[1]);
         }
-        return intervals;
+        return ans;
+
+
+        //sort(intervals.begin() , intervals.end());
+        //int i = 0;
+        //while(i < intervals.size()-1){
+        //    if(intervals[i][1] >= intervals[i+1][0]){
+        //        intervals[i][1] = max(intervals[i][1] , intervals[i+1][1]);
+        //        intervals.erase(intervals.begin() + (i+1));
+        //    } else i++;
+        //}
+        //return intervals;
     }
 };
