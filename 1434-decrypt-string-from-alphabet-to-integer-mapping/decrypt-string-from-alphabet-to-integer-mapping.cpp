af23b7cbed778fdbb48328b162e5a309df9cd884
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-13 19:15:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (37.86%) - LeetSync

--- 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.cpp
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     string freqAlphabets(string s) {
         string ans;
-        for(int i=s.length() ;  i>=0 ;){
+        for(int i=s.length()-1 ;  i>=0 ;){
             if(s[i] == '#'){
                 int n = stoi(s.substr(i-2 , 2));
                 i -= 3;
-                char c = (char)('a' + n - 1);
+                char c = 'a' + n - 1;
                 ans.push_back(c);
             } else {
                 int n = s[i] - '0';
-                char c = (char)('a' + n - 1);
+                char c = 'a' + n - 1;
                 ans.push_back(c);
                 i--;
             }
         }
         reverse(ans.begin() , ans.end());
-        ans.pop_back();
         return ans;
     }
 };
