2baee63ee8bc6f1c567c109d5abbdbee1d536e4b
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-14 10:52:59 +0530

    Time: 2 ms (90.00%) | Memory: 28.5 MB (100.00%) - LeetSync

--- 4415-count-values-with-equally-spaced-occurrences-i/count-values-with-equally-spaced-occurrences-i.cpp
+++ 4415-count-values-with-equally-spaced-occurrences-i/count-values-with-equally-spaced-occurrences-i.cpp
@@ -5,14 +5,35 @@ public:
         for(int i:nums) arr[i]++;
         int c = 0;
         for(int i=0 ; i<nums.size() ; i++){
-            for(int j=i+1 ; j<nums.size() ; j++){
-                for(int k=j+1 ; k<nums.size() ; k++){
-                    if(nums[i] == nums[j] && nums[i] == nums[k] && arr[nums[i]] == 3){
-                        c += (j - i == k - j);
+            int i1 = nums[i];
+            if(arr[i1] == 3){
+                int i2 = 0 , i3 = 0;
+                for(int j=i+1 ; j<nums.size() ; j++){
+                    if(nums[j] == i1){
+                        if(!i2) i2 = j;
+                        else i3 = j;
                     }
                 }
+                c += (i2 - i == i3 - i2);
             }
         }
         return c;
+
+
+
+
+        // vector<int> arr(101);
+        // for(int i:nums) arr[i]++;
+        // int c = 0;
+        // for(int i=0 ; i<nums.size() ; i++){
+        //     for(int j=i+1 ; j<nums.size() ; j++){
+        //         for(int k=j+1 ; k<nums.size() ; k++){
+        //             if(nums[i] == nums[j] && nums[i] == nums[k] && arr[nums[i]] == 3){
+        //                 c += (j - i == k - j);
+        //             }
+        //         }
+        //     }
+        // }
+        // return c;
     }
 };
