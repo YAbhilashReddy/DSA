0fbea5bdbda9633211fdabcc0b0f3343f941c22a
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-14 10:57:58 +0530

    Time: 6 ms (50.00%) | Memory: 32.1 MB (100.00%) - LeetSync

--- 4415-count-values-with-equally-spaced-occurrences-i/count-values-with-equally-spaced-occurrences-i.cpp
+++ 4415-count-values-with-equally-spaced-occurrences-i/count-values-with-equally-spaced-occurrences-i.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     int countSpecialIntegers(vector<int>& nums) {
-        vector<int> arr(101);
-        for(int i:nums) arr[i]++;
-        int c = 0;
+        vector<vector<int>> arr(101);
         for(int i=0 ; i<nums.size() ; i++){
-            int i1 = nums[i];
-            if(arr[i1] == 3){
-                int i2 = 0 , i3 = 0;
-                for(int j=i+1 ; j<nums.size() ; j++){
-                    if(nums[j] == i1){
-                        if(!i2) i2 = j;
-                        else i3 = j;
-                    }
-                }
-                c += (i2 - i == i3 - i2);
+            arr[nums[i]].push_back(i);
+        }
+        int c = 0;
+        for(int i=0 ; i<=100 ; i++){
+            if(arr[i].size() == 3){
+                c += (arr[i][1] - arr[i][0] == arr[i][2] - arr[i][1]);
             }
         }
         return c;
+
+
+
+        // vector<int> arr(101);
+        // for(int i:nums) arr[i]++;
+        // int c = 0;
+        // for(int i=0 ; i<nums.size() ; i++){
+        //     int i1 = nums[i];
+        //     if(arr[i1] == 3){
+        //         int i2 = 0 , i3 = 0;
+        //         for(int j=i+1 ; j<nums.size() ; j++){
+        //             if(nums[j] == i1){
+        //                 if(!i2) i2 = j;
+        //                 else i3 = j;
+        //             }
+        //         }
+        //         c += (i2 - i == i3 - i2);
+        //     }
+        // }
+        // return c;
 
 
 
