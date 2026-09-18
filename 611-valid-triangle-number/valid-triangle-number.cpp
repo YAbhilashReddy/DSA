6881b7dc417698aa3131775149bc651b11b6260b
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-18 07:23:21 +0530

    Time: 159 ms (17.95%) | Memory: 16.8 MB (9.96%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -2,15 +2,27 @@ class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
         sort(nums.begin() , nums.end());
-        int c = 0;
-        for(int i=0 ; i<nums.size() ; i++){
-            for(int j=i+1 ; j<nums.size() ; j++){
-                for(int k=j+1 ; k<nums.size() ; k++){
-                    bool a = nums[i] + nums[j] > nums[k];
-                    c += a;
-                }
+        int n = nums.size() , c = 0;
+        for(int i=0 ; i<n-2 ; i++){
+            for(int j=i+1 ; j<n-1 ; j++){
+                int ab = nums[i] + nums[j];
+                int k = lower_bound(nums.begin() + j + 1 , nums.end() , ab) - nums.begin();
+                c += k - j - 1;
             }
         }
         return c;
+
+
+        // sort(nums.begin() , nums.end());
+        // int c = 0;
+        // for(int i=0 ; i<nums.size() ; i++){
+        //     for(int j=i+1 ; j<nums.size() ; j++){
+        //         for(int k=j+1 ; k<nums.size() ; k++){
+        //             bool a = nums[i] + nums[j] > nums[k];
+        //             c += a;
+        //         }
+        //     }
+        // }
+        // return c;
     }
 };
