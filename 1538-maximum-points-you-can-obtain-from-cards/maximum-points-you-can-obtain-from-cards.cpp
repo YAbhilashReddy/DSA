211f9754949ee3f2f86bd2d17075d5891fb7830c
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-20 00:55:22 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (33.38%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
-        int n = cardPoints.size();
-        vector<int> prefix(n) , suffix(n);
-        for(int i=0 ; i<n ; i++) prefix[i] = (i ? prefix[i-1] : 0) + cardPoints[i];
-        for(int i=n-1 ; i>=0 ; i--) {
-            suffix[i] = (i == n-1 ? 0 : suffix[i+1]) + cardPoints[i];
-        } 
-        int maxi = INT_MIN;
-        for(int i=0 ; i<=k ; i++){
-            if(i == 0) maxi = max(maxi , suffix[n-k]);
-            else if(i == k) maxi = max(maxi , prefix[k-1]);
-            else {
-                int sum = prefix[i-1] + suffix[n - (k-i)];
-                maxi = max(maxi , sum);
-            }
+        int n = cardPoints.size() , sum = 0, maxi;
+        for(int i=0 ; i<k ; i++) sum += cardPoints[i];
+        maxi = sum;
+        int front = k-1 , back = n-1;
+        for(int i=0 ; i<k ; i++){
+            sum -= cardPoints[front--];
+            sum += cardPoints[back--];
+            maxi = max(maxi , sum);
         }
         return maxi;
+
+
+
+        // int n = cardPoints.size();
+        // vector<int> prefix(n) , suffix(n);
+        // for(int i=0 ; i<n ; i++) prefix[i] = (i ? prefix[i-1] : 0) + cardPoints[i];
+        // for(int i=n-1 ; i>=0 ; i--) {
+        //     suffix[i] = (i == n-1 ? 0 : suffix[i+1]) + cardPoints[i];
+        // } 
+        // int maxi = INT_MIN;
+        // for(int i=0 ; i<=k ; i++){
+        //     if(i == 0) maxi = max(maxi , suffix[n-k]);
+        //     else if(i == k) maxi = max(maxi , prefix[k-1]);
+        //     else {
+        //         int sum = prefix[i-1] + suffix[n - (k-i)];
+        //         maxi = max(maxi , sum);
+        //     }
+        // }
+        //return maxi;
     }
 };
