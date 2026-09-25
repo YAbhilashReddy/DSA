193390fbc4ef1e007297b013aef02977cd23a578
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-25 06:56:56 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (81.20%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int countPartitions(vector<int>& nums) {
-        int n = nums.size() , c = 0;
-        vector<int> prefix(n) , suffix(n);
-        for(int i=0 ; i<n ; i++) prefix[i] = (i ? prefix[i-1] : 0) + nums[i];
-        for(int i=n-1 ; i>=0 ; i--) suffix[i] = (i == n-1 ? 0 : suffix[i+1]) + nums[i];
-        for(int i=1 ; i<n ; i++){
-            int diff = abs(prefix[i-1] - suffix[i]);
-            if(diff % 2 == 0) c++;
+        int n = nums.size() ,c = 0;
+        for(int i=1 ; i<nums.size() ; i++) nums[i] += nums[i-1];
+        n--;
+        for(int i=0 ; i<n ; i++){
+            int left = nums[i] , right = nums[n] - nums[i];
+            c += !(abs(left - right) % 2);
         }
         return c;
+
+
+
+        // int n = nums.size() , c = 0;
+        // vector<int> prefix(n) , suffix(n);
+        // for(int i=0 ; i<n ; i++) prefix[i] = (i ? prefix[i-1] : 0) + nums[i];
+        // for(int i=n-1 ; i>=0 ; i--) suffix[i] = (i == n-1 ? 0 : suffix[i+1]) + nums[i];
+        // for(int i=1 ; i<n ; i++){
+        //     int diff = abs(prefix[i-1] - suffix[i]);
+        //     if(diff % 2 == 0) c++;
+        // }
+        // return c;
     }
 };
