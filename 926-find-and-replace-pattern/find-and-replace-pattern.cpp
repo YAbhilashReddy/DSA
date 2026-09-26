3a0d0ddab2afb03f22fed8e33594ad3f46c690a3
ABHILASH REDDY YANAGUNDE <141263546+YAbhilashReddy@users.noreply.github.com>
2026-09-26 09:42:41 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (91.00%) - LeetSync

--- 926-find-and-replace-pattern/find-and-replace-pattern.cpp
+++ 926-find-and-replace-pattern/find-and-replace-pattern.cpp
@@ -1,24 +1,51 @@
 class Solution {
 public:
-    vector<int> wordPattern(string s){
-        if(s.empty()) return {};
-        vector<int> res;
-        unordered_map<char,int> freq;
-        int idx = 0;
+    string strPattern(string s){
+        if(s.empty()) return "";
+        string res = "";
+        vector<int> arr(26 , 0);
+        int setBit = 1;
         for(char c:s){
-            if(freq.find(c) == freq.end()) freq[c] = idx , idx++ , res.push_back(freq[c]);
-            else res.push_back(freq[c]);
+            if(!arr[c - 'a']) arr[c - 'a'] = setBit++;
+            res.push_back(arr[c - 'a'] + '0');
         }
         return res;
     }
 
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
         vector<string> ans;
-        vector<int> patternCode = wordPattern(pattern);
+        string patternCode = strPattern(pattern);
         for(string word:words){
-            vector<int> wordCode = wordPattern(word);
-            if(wordCode == patternCode) ans.push_back(word);
+            if(patternCode == strPattern(word)) ans.push_back(word);
         }
         return ans;
     }
+
+
+
+
+
+    // vector<int> wordPattern(string s){
+    //     if(s.empty()) return {};
+    //     vector<int> res;
+    //     unordered_map<char,int> freq;
+    //     int idx = 0;
+    //     for(char c:s){
+    //         if(freq.find(c) == freq.end()) freq[c] = idx , idx++ , res.push_back(freq[c]);
+    //         else res.push_back(freq[c]);
+    //     }
+    //     return res;
+    // }
+
+    // vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+    //     vector<string> ans;
+    //     vector<int> patternCode = wordPattern(pattern);
+    //     for(string word:words){
+    //         vector<int> wordCode = wordPattern(word);
+    //         if(wordCode == patternCode) ans.push_back(word);
+    //     }
+    //     return ans;
+    // }
+
+
 };
